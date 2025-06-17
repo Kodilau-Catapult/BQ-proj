9714a649f7b6243d6c3b6b999008eeda300c0f0e
Kodi Lau <klau27@student.signature.edu>
2025-06-17 00:11:10 -0500

    BQedit1

---
+++ BQ.js
@@ -0,0 +1,33 @@
+//js to css 
+
+let button = document.querySelector(".option");
+let option1 = document.querySelector(".option1");
+let option2 = document.querySelector(".option2");
+let option3 = document.querySelector(".option3");
+let option4 = document.querySelector(".option4");
+
+//point system
+
+let counter1;
+let counter2;
+let counter3;
+let counter4; 
+
+
+//results 
+//Brr Brr Patapim
+//Tung Tung Tung Sahur 
+//Insert 3rd
+//Insert 4th 
+
+
+
+option1.addEventListener("click", function(){
+
+counter1 += 5
+
+
+})
+
+
+
